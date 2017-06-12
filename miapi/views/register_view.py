0d874e4b529687943d7c30df77f8e874ae9848d5
William Caldwell <wocaldwell@gmail.com>
2017-06-12 15:18:34 -0500

    user can login or register

---
+++ miapi/views/register_view.py
@@ -0,0 +1,45 @@
+from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
+from rest_framework.authtoken.models import Token
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+@csrf_exempt
+def register_user(request):
+    '''Handles the creation of a new user for authentication
+
+    Method arguments:
+      request -- The full HTTP request object
+    '''
+
+    # Load the JSON string of the request body into a dict
+    req_body = json.loads(request.body.decode())
+
+    # Create a new user by invoking the `create_user` helper method
+    # on Django's built-in User model
+    new_user = User.objects.create_user(
+                    username=req_body['username'],
+                    password=req_body['password'],
+                    email=req_body['email'],
+                    )
+
+    # Commit the user to the database by saving it
+
+    new_user.save()
+
+    # Use the REST Framework's token generator on the new user account
+    token = Token.objects.create(user=new_user)
+
+    # Return the token to the client
+    data = json.dumps({"token":token.key})
+    return HttpResponse(data, content_type='application/json')
+
+
+
+
+
+
+
+
+
+
---
+++ minylapi/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from miapi import views
+from miapi.views import register_view
+
+router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^register/', register_view.register_user),
+    url(r'^api-token-auth/', obtain_auth_token),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
